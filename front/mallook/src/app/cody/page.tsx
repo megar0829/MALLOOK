89f5299591ab181dd61864e95a143728a2925e45
megar0829 <megar0829@gmail.com>
2024-02-29 17:04:20 +0900

    [FE] Create Project & Setting

---
+++ front/mallook/src/app/cody/page.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export const metadata = {
+  title: "코디",
+};
+
+export default async function CodyPage() {
+
+  return (
+    <div>
+      cody page
+    </div>
+  );
+}
---
+++ front/mallook/src/app/worldcup/page.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export const metadata = {
+  title: "월드컵",
+};
+
+export default async function WorldCupPage() {
+
+  return (
+    <div>
+      WorldCup page
+    </div>
+  );
+}
