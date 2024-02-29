dc35b96c35a92b0ed5b5a7b2b36e2d9a72b4f686
duckbill413 <uhyeon7399@daum.net>
2024-02-29 14:03:24 +0900

    :sparkles: [BE] 초기 백엔드 프로젝트 생성

---
+++ back/mallook/src/main/java/io/ssafy/mallook/MallookApplication.java
@@ -0,0 +1,13 @@
+package io.ssafy.mallook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MallookApplication.class, args);
+	}
+
+}
---
+++ back/mallook/src/test/java/io/ssafy/mallook/MallookApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ssafy.mallook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
