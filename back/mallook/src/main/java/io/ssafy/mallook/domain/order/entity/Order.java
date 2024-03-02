34e2659725596175e8e7f3ebc379fc2035c623cb
dlguddn3024 <dlguddn3024@gmail.com>
2024-03-03 02:20:58 +0900

    :recycle: [BE] refactor order entity

--- back/mallook/src/main/java/io/ssafy/mallook/domain/order/entity/Order.java
+++ back/mallook/src/main/java/io/ssafy/mallook/domain/order/entity/Order.java
@@ -1,6 +1,7 @@
 package io.ssafy.mallook.domain.order.entity;
 
 import io.ssafy.mallook.domain.BaseEntity;
+import io.ssafy.mallook.domain.member.entity.Member;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -10,16 +11,20 @@ import lombok.*;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name="order")
+@Table(name = "orders")
 public class Order extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member memberId;
+
     private Long totalPrice;
 
-    private Long  totalFee;
+    private Long totalFee;
 
-    private Long  totalCount;
+    private Long totalCount;
 }
