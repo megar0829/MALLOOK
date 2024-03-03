ecd1e5a67f4f0e4932cf830878ab847364b91094
duckbill413 <uhyeon7399@daum.net>
2024-03-03 17:37:39 +0900

    :sparkles: [APP] localization english to korean

--- app/mallook/lib/main.dart
+++ app/mallook/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:mallook/feature/main_navigation/main_navigation_screen.dart';
+import 'package:mallook/feature/sign_up/sign_up_screen.dart';
 
 void main() {
   runApp(const Mallook());
@@ -7,15 +9,28 @@ void main() {
 
 class Mallook extends StatelessWidget {
   const Mallook({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Mallook',
       theme: ThemeData(
+        primaryColor: Colors.purple,
+        disabledColor: Colors.grey.shade300,
+        hintColor: Colors.black54,
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const MainNavigationScreen(),
+      home: const SignUpScreen(),
+      // home: const MainNavigationScreen(),
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('ko', 'KR'),
+      ],
     );
   }
 }
