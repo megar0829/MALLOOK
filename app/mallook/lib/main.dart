374c4ca8576e4df8454987c09bbec4a1d37eff28
duckbill413 <uhyeon7399@daum.net>
2024-03-08 11:09:50 +0900

    :: [APP] login package update

--- app/mallook/lib/main.dart
+++ app/mallook/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:mallook/feature/sign_up/sign_up_screen.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
+import 'package:mallook/feature/login/LoginScreen.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
