62f62c74713e235fc7b311360e42f6a00dcd46dc
Leehyoungwoo <dlguddn3024@gmail.com>
2024-03-12 14:49:24 +0900

    :recycle: remove none use import

--- back/mallook/src/test/java/io/ssafy/mallook/domain/shoppingmall/api/ShoppingMallControllerTest.java
+++ back/mallook/src/test/java/io/ssafy/mallook/domain/shoppingmall/api/ShoppingMallControllerTest.java
@@ -13,13 +13,9 @@ import org.springframework.http.MediaType;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @WebMvcTest(controllers = ShoppingMallController.class,
         excludeFilters = {
@@ -41,7 +37,7 @@ class ShoppingMallControllerTest {
         Pageable pageable = PageRequest.of(0, 20);
 
         mockMvc.perform(
-                MockMvcRequestBuilders.get(url))
+                        MockMvcRequestBuilders.get(url))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("SELECT SUCCESS"));
