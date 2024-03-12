13a323be2c7e5c8658149d16c33a8a7880506126
dlguddn3024 <dlguddn3024@gmail.com>
2024-03-12 21:51:38 +0900

    :sparkles: test OrderController APIs

---
+++ back/mallook/src/test/java/io/ssafy/mallook/domain/order/api/OrderControllerTest.java
@@ -0,0 +1,122 @@
+package io.ssafy.mallook.domain.order.api;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.ssafy.mallook.config.security.WithMockCustomUser;
+import io.ssafy.mallook.domain.order.application.OrderService;
+import io.ssafy.mallook.domain.order.dto.request.OrderCreateDto;
+import io.ssafy.mallook.domain.order.dto.request.OrderDeleteDto;
+import io.ssafy.mallook.domain.order.dto.response.OrderDetailDto;
+import io.ssafy.mallook.domain.order.dto.response.OrderListDto;
+import io.ssafy.mallook.domain.product.api.ProductController;
+import io.ssafy.mallook.domain.product.dto.response.ProductListDto;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+
+@WebMvcTest(controllers = OrderController.class,
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebMvcConfigurer.class),
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebSecurityConfigurer.class)})
+class OrderControllerTest {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private OrderService orderService;
+
+    private String url = "/api/orders";
+
+    @Test
+    @WithMockCustomUser(id = "123e4567-e89b-12d3-a456-426614174000", role = "USER")
+    void getOrderList() throws Exception {
+        UUID id = UUID.randomUUID();
+        List<OrderListDto> list = new ArrayList<>();
+        Pageable pageable = PageRequest.of(0, 2);
+        Page<OrderListDto> page = new PageImpl<>(list, pageable, list.size());
+        Mockito.when(orderService.getOrderList(any(UUID.class), eq(pageable)))
+                .thenReturn(page);
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders.get(url)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("SELECT SUCCESS"));
+    }
+
+    @Test
+    @WithMockCustomUser(id = "123e4567-e89b-12d3-a456-426614174000", role = "USER")
+    void getOrderDetail() throws Exception {
+        Long orderId = 1L;
+        OrderDetailDto orderDetailDto = new OrderDetailDto(1000L, 100L, 10L);
+        Mockito.when(orderService.getOrderDetail(orderId))
+                .thenReturn(orderDetailDto);
+
+        mockMvc.perform(MockMvcRequestBuilders.get(url + "/{id}", orderId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.result").exists())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("SELECT SUCCESS"));
+    }
+
+    @Test
+    @WithMockCustomUser(id = "123e4567-e89b-12d3-a456-426614174000", role = "USER")
+    void createOrder() throws Exception {
+        OrderCreateDto orderCreateDto = new OrderCreateDto(1000L, 100L, 10L);
+
+        mockMvc.perform(MockMvcRequestBuilders.post(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(orderCreateDto))
+                .with(csrf()))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("INSERT SUCCESS")
+                );
+    }
+
+    @Test
+    @WithMockCustomUser(id = "123e4567-e89b-12d3-a456-426614174000", role = "USER")
+    void deleteOrder() throws Exception {
+        OrderDeleteDto orderDeleteDto = new OrderDeleteDto(new ArrayList<>());
+
+        mockMvc.perform(MockMvcRequestBuilders.delete(url)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(orderDeleteDto))
+                        .with(csrf()))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("DELETE SUCCESS"));
+
+    }
+}
