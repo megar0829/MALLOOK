3620517b7a2e105b18f2553785e95ed5419d04de
Leehyoungwoo <dlguddn3024@gmail.com>
2024-03-12 14:31:45 +0900

    :sparkles: test ShoppingMall GET Request Test

---
+++ back/mallook/src/test/java/io/ssafy/mallook/domain/shoppingmall/api/ShoppingMallControllerTest.java
@@ -0,0 +1,50 @@
+package io.ssafy.mallook.domain.shoppingmall.api;
+
+import io.ssafy.mallook.domain.shoppingmall.application.ShoppingMallService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@WebMvcTest(controllers = ShoppingMallController.class,
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebMvcConfigurer.class),
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebSecurityConfigurer.class)})
+class ShoppingMallControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    private ShoppingMallService shoppingMallService;
+
+
+    private String url = "/api/shopping-mall";
+
+    @Test
+    @WithMockUser
+    void getAllShoppingMallList() throws Exception {
+        Pageable pageable = PageRequest.of(0, 20);
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.get(url))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("SELECT SUCCESS"));
+    }
+}
