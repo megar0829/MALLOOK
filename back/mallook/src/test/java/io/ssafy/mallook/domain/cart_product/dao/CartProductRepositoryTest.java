ab9f0f40c47b61e0d864450dbe1cddfffbfc6e87
wjstp <wjstp14@gmail.com>
2024-03-14 16:16:19 +0900

    :recycle: refactor cart api

--- back/mallook/src/test/java/io/ssafy/mallook/domain/cart_product/dao/CartProductRepositoryTest.java
+++ back/mallook/src/test/java/io/ssafy/mallook/domain/cart_product/dao/CartProductRepositoryTest.java
@@ -13,6 +13,7 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +23,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.*;
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles(profiles = "dev")
 class CartProductRepositoryTest {
 
     @Autowired
--- back/mallook/src/test/java/io/ssafy/mallook/domain/style/dao/StyleRepositoryTest.java
+++ back/mallook/src/test/java/io/ssafy/mallook/domain/style/dao/StyleRepositoryTest.java
@@ -14,6 +14,7 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,6 +25,7 @@ import static org.assertj.core.api.Assertions.*;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles(profiles = "dev")
 class StyleRepositoryTest {
 
     @Autowired
