f988731bc7b845bd0e81d53d39fa3569a38581d4
duckbill413 <uhyeon7399@daum.net>
2024-03-18 16:06:39 +0900

    :sparkles: [APP] appbar keyword search ListView

---
+++ app/mallook/lib/feature/search/search_product_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SearchProductScreen extends StatelessWidget {
+  final String? keyword;
+
+  const SearchProductScreen({super.key, this.keyword});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+
+    );
+  }
+}
