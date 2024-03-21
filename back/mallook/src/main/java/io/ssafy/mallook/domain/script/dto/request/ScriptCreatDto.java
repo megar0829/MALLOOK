bbe500f8ddc0d29a954a051a9c875b81aedfb777
Leehyoungwoo <dlguddn3024@gmail.com>
2024-03-21 14:15:01 +0900

    :sparkles: add annotation for data integrity

--- back/mallook/src/main/java/io/ssafy/mallook/domain/script/dto/request/ScriptCreatDto.java
+++ back/mallook/src/main/java/io/ssafy/mallook/domain/script/dto/request/ScriptCreatDto.java
@@ -4,6 +4,7 @@ import io.ssafy.mallook.domain.keyword.entity.Keyword;
 import io.ssafy.mallook.domain.member.entity.Member;
 import io.ssafy.mallook.domain.script.entity.Script;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
@@ -11,6 +12,7 @@ import java.util.List;
 
 @Builder
 public record ScriptCreatDto(
+        @NotEmpty
         List<String> keywordsList) {
 
     public Script toEntity(Member member, String scriptContent) {
