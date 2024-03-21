efb396f4847ff971c9a4ab2d68f468b0d05984b2
Leehyoungwoo <dlguddn3024@gmail.com>
2024-03-21 17:58:24 +0900

    :sparkles: teaching gpt by prompt

--- back/mallook/src/main/java/io/ssafy/mallook/domain/chatgpt/prompt/Prompt.java
+++ back/mallook/src/main/java/io/ssafy/mallook/domain/chatgpt/prompt/Prompt.java
@@ -3,11 +3,11 @@ package io.ssafy.mallook.domain.chatgpt.prompt;
 public class Prompt {
 
     public static String generateQuestionPrompt(String question) {
-        String strict = "맨 앞의 키워드는 반드시 들어가거나 비슷한 의미의 단어로 대치해야해." +
-                " 물론이죠! 나 감탄사를 넣지마." +
+        String strict = "내가 주는 키워드는 반드시 들어가거나 비슷한 의미의 단어로 대치해야해." +
+                "명령이야 따옴표 절대 넣지마" +
+                " 물론이죠! 나 감탄사와 따옴표를 넣지마" +
                 " 그냥 한 문장의 느낌을 내고 싶어." +
                 "비문이 없어야 해" +
-                "그리고 쌍 따옴표를 넣지말고 대답해" +
                 "단순한 단어의 나열이 아닌, 완성된 문장이 나와야해.";
         return String.format("\"%s한 코디가 당신에게 어울려요\" 와 비슷한 문장으로 끝나게 답변을 받고 싶어. %s", question, strict);
     }
