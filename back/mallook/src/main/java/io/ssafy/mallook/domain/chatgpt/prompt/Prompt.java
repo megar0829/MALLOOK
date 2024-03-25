06b652b7830c485ac2c37dec0fa7217cf3b5907d
Leehyoungwoo <dlguddn3024@gmail.com>
2024-03-25 10:51:54 +0900

    :recycle: refactor prompt scripts

--- back/mallook/src/main/java/io/ssafy/mallook/domain/chatgpt/prompt/Prompt.java
+++ back/mallook/src/main/java/io/ssafy/mallook/domain/chatgpt/prompt/Prompt.java
@@ -3,12 +3,12 @@ package io.ssafy.mallook.domain.chatgpt.prompt;
 public class Prompt {
 
     public static String generateQuestionPrompt(String question) {
-        String strict = "내가 주는 키워드는 반드시 들어가거나 비슷한 의미의 단어로 대치해야해." +
-                "명령이야 따옴표 절대 넣지마" +
-                " 물론이죠! 와 같은 감탄사를 넣지마" +
-                " 그냥 한 문장의 느낌을 내고 싶어." +
-                "비문이 없어야 해" +
-                "단순한 단어의 나열이 아닌, 완성된 문장이 나와야해.";
+        String strict = "맨 앞의 키워드는 반드시 들어가거나 유사한 의미의 단어로 대치해야해." +
+                " 물론이죠! 나 감탄사를 넣지마. \n" +
+                " 그냥 한 문장의 느낌을 내고 싶어. \n" +
+                "비문이 없어야 해. \n" +
+                "Never put double quotes in your answer, just give the sentence.\n" +
+                "단순한 단어의 나열이 아닌, 자연스러운 문장이 나와야해. \n";
         return String.format("\"%s한 코디가 당신에게 어울려요\" 와 비슷한 문장으로 끝나게 답변을 받고 싶어. %s", question, strict);
     }
 }
