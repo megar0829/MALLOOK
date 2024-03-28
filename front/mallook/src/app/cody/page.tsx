c9e3b5d51c7827f8c505d1fe58e5b0613affbf77
megar0829 <megar0829@gmail.com>
2024-03-28 17:54:45 +0900

    :sparkles: [FE] Update - Product Detail Page

--- front/mallook/src/app/cody/page.tsx
+++ front/mallook/src/app/cody/page.tsx
@@ -1,11 +1,14 @@
 import Link from "next/link";
 import styles from "./cody.module.css";
 
+import iconHanger from "@/assets/img/icons/hanger.png";
+
 export const metadata:{title: string} = {
   title: "코디",
 };
 
 import Masonry from "@/app/cody/_components/Masonry";
+import Image from "next/image";
 
 
 export default function CodyPage() {
@@ -13,6 +16,11 @@ export default function CodyPage() {
   return (
     <div className={styles.container}>
       <Masonry />
+      <Link href={"/cody/create"}>
+        <div className={styles.iconDiv}>
+          <Image className={styles.iconImage} src={iconHanger} alt="옷걸이 아이콘" />
+        </div>
+      </Link>
     </div>
   );
 }
