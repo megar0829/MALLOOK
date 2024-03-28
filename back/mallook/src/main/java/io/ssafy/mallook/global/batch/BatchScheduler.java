fbc1f183bfa58bf1a7edb6aad1d37cbc7db7ee78
Leehyoungwoo <dlguddn3024@gmail.com>
2024-03-28 09:36:40 +0900

    :recycle: refactor batch job execute by server role

--- back/mallook/src/main/java/io/ssafy/mallook/global/batch/BatchScheduler.java
+++ back/mallook/src/main/java/io/ssafy/mallook/global/batch/BatchScheduler.java
@@ -1,7 +1,7 @@
 package io.ssafy.mallook.global.batch;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import lombok.extern.log4j.Log4j2;
 import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParametersBuilder;
@@ -18,7 +18,7 @@ import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 
-@Slf4j
+@Log4j2
 @RequiredArgsConstructor
 @Component
 public class BatchScheduler {
@@ -32,6 +32,10 @@ public class BatchScheduler {
     @SchedulerLock(name = "couponTask", lockAtLeastFor = "50s", lockAtMostFor = "10m")
     public void runJob() {
         String time = LocalDateTime.now().toString();
+        if (!(serverRole.equals("batch"))) {
+            log.info("현재 서버 역할이 '{}'로 설정되어 있어, 'gradeJob' 작업을 수행하지 않습니다.", serverRole);
+            return;
+        }
         try {
             Job job = jobRegistry.getJob("gradeJob");
             JobParametersBuilder jobParameter = new JobParametersBuilder().addString("time", time);
