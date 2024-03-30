6e9ac055ea3743d05550440e7821fa549d62805a
dlguddn3024 <dlguddn3024@gmail.com>
2024-03-30 15:18:30 +0900

    :sparkles: feat tasklet batch

--- back/mallook/src/main/java/io/ssafy/mallook/global/batch/config/HeartBatchConfig.java
+++ back/mallook/src/main/java/io/ssafy/mallook/global/batch/config/HeartBatchConfig.java
@@ -1,20 +1,22 @@
 package io.ssafy.mallook.global.batch.config;
 
-import io.ssafy.mallook.domain.heart.script_heart.dao.ScriptHeartRepository;
-import io.ssafy.mallook.domain.heart.style_heart.dao.StyleHeartRepository;
 import io.ssafy.mallook.domain.script.dao.ScriptRepository;
 import io.ssafy.mallook.domain.script.entity.Script;
+import io.ssafy.mallook.domain.style.dao.StyleRepository;
+import io.ssafy.mallook.domain.style.entity.Style;
 import io.ssafy.mallook.global.batch.dao.Top50RedisDao;
 import io.ssafy.mallook.global.batch.dto.TopScriptDto;
-import io.ssafy.mallook.global.batch.tasklet.UpdateRedisValueTasklet;
-import jakarta.persistence.EntityManagerFactory;
+import io.ssafy.mallook.global.batch.dto.TopStyleDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -26,29 +28,57 @@ import java.util.List;
 @RequiredArgsConstructor
 public class HeartBatchConfig {
 
-    private final int CHUNK_SIZE = 1000;
-    private final String JOB_NAME = "memberJob";
-    private final Top50RedisDao top50RedisDao;
-    private final EntityManagerFactory entityManagerFactory;
+    private final StyleRepository styleRepository;
     private final ScriptRepository scriptRepository;
-    private final StyleHeartRepository styleHeartRepository;
-    private final UpdateRedisValueTasklet updateRedisValueTasklet;
+    private final Top50RedisDao top50RedisDao;
 
     @Bean
     public Job getHeartInitJob(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
         return new JobBuilder("heartInitJob", jobRepository)
-                .start(changeTopScript())
+                .incrementer(new RunIdIncrementer())
+                .start(changeTopScript(jobRepository, transactionManager))
+                .next(changeTopStyle(jobRepository, transactionManager))
+                .build();
     }
 
     @Bean
     public Step changeTopScript(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
-        List<Script> topScript = scriptRepository.findTop50ScriptsOrderByTotalLikeDesc();
-        TopScriptDto scriptDto = TopScriptDto.builder()
-                .scripts(topScript)
+        return new StepBuilder("changeScriptStep", jobRepository)
+                .tasklet(changeScriptTask(), transactionManager)
                 .build();
-        top50RedisDao.saveScripts(scriptDto, 35);
-        return new StepBuilder(jobRepository, transactionManager)
-                .tasklet(updateRedisValueTasklet)
+    }
+
+    @Bean
+    public Step changeTopStyle(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new StepBuilder("changeStyleStep", jobRepository)
+                .tasklet(changeStyleTask(), transactionManager)
                 .build();
+    }
+
+    @Bean
+    public Tasklet changeScriptTask() {
+        return (contribution, chunkContext) -> {
+            List<Script> topScript = scriptRepository.findTop50ScriptsOrderByTotalLikeDesc();
+            TopScriptDto scriptDto = TopScriptDto.builder()
+                    .scripts(topScript)
+                    .build();
+            top50RedisDao.saveScripts(scriptDto, 35);
+
+            log.info("스크립트 초기화 작업 완료");
+            return RepeatStatus.FINISHED;
+        };
+    }
+
+    private Tasklet changeStyleTask() {
+        return (contribution, chunkContext) -> {
+            List<Style> topStyle = styleRepository.findTop50StylesOrderByTotalLikeDesc();
+            TopStyleDto styleDto = TopStyleDto.builder()
+                    .styles(topStyle)
+                    .build();
+            top50RedisDao.saveStyles(styleDto, 35);
+
+            log.info("스크립트 초기화 작업 완료");
+            return RepeatStatus.FINISHED;
+        };
     }
 }
--- back/mallook/src/main/java/io/ssafy/mallook/global/batch/dto/TopStyleDto.java
+++ back/mallook/src/main/java/io/ssafy/mallook/global/batch/dto/TopStyleDto.java
@@ -1,9 +1,11 @@
 package io.ssafy.mallook.global.batch.dto;
 
 import io.ssafy.mallook.domain.style.entity.Style;
+import lombok.Builder;
 
 import java.util.List;
 
+@Builder
 public record TopStyleDto(
         List<Style> styles
 ) {
