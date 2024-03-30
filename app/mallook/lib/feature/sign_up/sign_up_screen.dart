19d90120f6a81f47cac7450b7d1b7d9a95076607
duckbill413 <uhyeon7399@daum.net>
2024-03-30 22:01:12 +0900

    :recycle: [APP] nickname api test

--- app/mallook/lib/feature/sign_up/sign_up_screen.dart
+++ app/mallook/lib/feature/sign_up/sign_up_screen.dart
@@ -28,7 +28,6 @@ class _SignUpScreenState extends State<SignUpScreen> {
       TextEditingController();
   DateTime? _currentBackPressTime;
   String _nickname = "";
-  bool _nicknameStatus = false;
   String _phone = "";
   bool _phoneStatus = false;
   late DateTime _birthDate;
@@ -43,7 +42,6 @@ class _SignUpScreenState extends State<SignUpScreen> {
   late RandomNicknameModel randomNicknameModel;
 
   bool _isAvailable() {
-    if (!_nicknameStatus) return false;
     if (!_phoneStatus) return false;
     if (_gender == null) return false;
     if (kopoModel == null) return false;
@@ -59,7 +57,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
         _nickname = _nicknameController.text;
       });
     });
-    setRandomNickname();
+    _setRandomNickname();
 
     _phoneController.text = "010-";
     _phoneController.addListener(() {
@@ -95,13 +93,10 @@ class _SignUpScreenState extends State<SignUpScreen> {
     super.dispose();
   }
 
-  void setRandomNickname() async {
+  void _setRandomNickname() async {
     randomNicknameModel = await SignupApiService.getRandomNickname();
     if (randomNicknameModel.nickname != null) {
       _nickname = _nicknameController.text = randomNicknameModel.nickname!;
-      setState(() {
-        _nicknameStatus = true;
-      });
     }
   }
 
@@ -110,12 +105,6 @@ class _SignUpScreenState extends State<SignUpScreen> {
       _year = dateTime.year;
       _month = dateTime.month;
       _day = dateTime.day;
-    });
-  }
-
-  void _isNicknameValid() {
-    setState(() {
-      _nicknameStatus = true;
     });
   }
 
@@ -620,7 +609,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
               ),
             ),
             Text(
-              "입력하지 않으면 자동으로 생성되요",
+              "닉네임 추천 받을까요?",
               style: TextStyle(
                 color: Colors.grey.shade600,
                 fontSize: Sizes.size14,
@@ -661,7 +650,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
               ),
             ),
             ElevatedButton(
-              onPressed: _isNicknameValid,
+              onPressed: _setRandomNickname,
               style: ElevatedButton.styleFrom(
                 backgroundColor: Theme.of(context).primaryColorLight,
                 shape: RoundedRectangleBorder(
@@ -674,7 +663,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
                 ),
               ),
               child: Text(
-                "중복검사",
+                "재생성",
                 style: TextStyle(
                   color: Theme.of(context).primaryColorDark,
                   fontWeight: FontWeight.w500,
@@ -684,18 +673,6 @@ class _SignUpScreenState extends State<SignUpScreen> {
             )
           ],
         ),
-        Gaps.v3,
-        if (_nickname.isNotEmpty && !_nicknameStatus)
-          Text(
-            _nicknameStatus ? "사용 가능한 닉네임입니다." : "사용할 수 없는 닉네임 입니다.",
-            style: TextStyle(
-              fontSize: Sizes.size14,
-              color: _nicknameStatus
-                  ? Theme.of(context).primaryColorDark
-                  : Colors.red.shade600,
-              fontWeight: FontWeight.w500,
-            ),
-          )
       ],
     );
   }
