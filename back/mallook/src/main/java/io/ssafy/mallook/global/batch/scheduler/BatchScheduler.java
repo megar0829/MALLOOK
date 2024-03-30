4185d7e1e6dbabb610cd9993889efd61e411e3ae
dlguddn3024 <dlguddn3024@gmail.com>
2024-03-31 03:38:22 +0900

    :recycle: refactor conditional active batch

--- back/mallook/src/main/java/io/ssafy/mallook/global/batch/scheduler/BatchScheduler.java
+++ back/mallook/src/main/java/io/ssafy/mallook/global/batch/scheduler/BatchScheduler.java
@@ -13,6 +13,7 @@ import org.springframework.batch.core.repository.JobExecutionAlreadyRunningExcep
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -21,6 +22,7 @@ import java.time.LocalDateTime;
 @Log4j2
 @RequiredArgsConstructor
 @Component
+@Profile("batch")
 public class BatchScheduler {
 
     @Value("${server.role}")
