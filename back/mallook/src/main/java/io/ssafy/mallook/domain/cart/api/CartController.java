1f327f4bf6bda5183e9c58abc1424d9d39452c05
wjstp <wjstp14@gmail.com>
2024-04-03 01:20:56 +0900

    :recycle:[BE] fix cart api

--- back/mallook/src/main/java/io/ssafy/mallook/domain/cart/api/CartController.java
+++ back/mallook/src/main/java/io/ssafy/mallook/domain/cart/api/CartController.java
@@ -1,9 +1,9 @@
 package io.ssafy.mallook.domain.cart.api;
 
 import io.ssafy.mallook.domain.cart.application.CartService;
-import io.ssafy.mallook.domain.cart.dto.request.CartProductDeleteReq;
 import io.ssafy.mallook.domain.cart.dto.request.CartInsertReq;
-import io.ssafy.mallook.domain.cart.dto.response.CartPageRes;
+import io.ssafy.mallook.domain.cart.dto.request.CartProductDeleteReq;
+import io.ssafy.mallook.domain.cart.dto.response.CartDetailRes;
 import io.ssafy.mallook.global.common.BaseResponse;
 import io.ssafy.mallook.global.common.code.SuccessCode;
 import io.ssafy.mallook.global.security.user.UserSecurityDTO;
@@ -12,12 +12,11 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/carts")
@@ -33,11 +32,10 @@ public class CartController {
                     @ApiResponse(responseCode = "404", description = "장바구니 조회 실패")
             })
     @GetMapping
-    public ResponseEntity<BaseResponse<CartPageRes>> findProductsInCart(
-            @AuthenticationPrincipal UserSecurityDTO userSecurityDTO,
-            @PageableDefault(size = 20, direction = Sort.Direction.DESC, page = 0) Pageable pageable
+    public ResponseEntity<BaseResponse<List<CartDetailRes>>> findProductsInCart(
+            @AuthenticationPrincipal UserSecurityDTO userSecurityDTO
     ) {
-        var result = cartService.findProductsInCart(pageable, userSecurityDTO.getId());
+        var result = cartService.findProductsInCart(userSecurityDTO.getId());
         return BaseResponse.success(
                 SuccessCode.SELECT_SUCCESS,
                 result
@@ -75,6 +73,7 @@ public class CartController {
                 "장바구니 내 상품 삭제 성공"
         );
     }
+
     @Operation(summary = "장바구니 전체 삭제",
             responses = {
                     @ApiResponse(responseCode = "200", description = "장바구니 전체 삭제 성공"),
