cb619c7ffd4641eeefb91f51ec61fec1c5d37aef
wjstp <wjstp14@gmail.com>
2024-04-04 03:03:15 +0900

    :recycle:[BE] fix style dto

--- back/mallook/src/main/java/io/ssafy/mallook/domain/style/dto/response/StyleProductRes.java
+++ back/mallook/src/main/java/io/ssafy/mallook/domain/style/dto/response/StyleProductRes.java
@@ -4,6 +4,8 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "스타일에 포함된 상품 정보 요청시 응답 DTO")
 public record StyleProductRes(
+        @Schema(description = "상품 id")
+        String productsId,
         @Schema(description="상품명")
         String name,
         @Schema(description="상품 가격")
